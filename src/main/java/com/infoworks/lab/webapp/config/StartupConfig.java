6a9da995e97dff7b75ae6b7030465142edd1cac0
m.towhid.islam <m.towhid.islam@gmail.com>
2025-03-20 23:53:12 +0100

    fix: Updated

--- src/main/java/com/infoworks/lab/webapp/config/StartupConfig.java
+++ src/main/java/com/infoworks/lab/webapp/config/StartupConfig.java
@@ -1,5 +1,6 @@
 package com.infoworks.lab.webapp.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.event.ContextClosedEvent;
 import org.springframework.context.event.ContextRefreshedEvent;
@@ -9,9 +10,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class StartupConfig implements CommandLineRunner {
 
+    @Value("${server.port}")
+    private String serverPort;
+
     @Override
     public void run(String... args) throws Exception {
         //How to use executor:
+        System.out.println(String.format("http://localhost:%s/swagger-ui/index.html", serverPort));
         //
     }
 
