b51e89db4d0bc4b11fa1af524e1374a9a9808d0f
m.towhid.islam <m.towhid.islam@gmail.com>
2025-03-26 01:38:12 +0100

    fix: need to test.

--- src/main/java/com/infoworks/lab/domain/repositories/ElasticSearchableRepository.java
+++ src/main/java/com/infoworks/lab/domain/repositories/ElasticSearchableRepository.java
@@ -1,11 +1,9 @@
 package com.infoworks.lab.domain.repositories;
 
 import com.infoworks.lab.rest.models.SearchQuery;
-import com.infoworks.lab.rest.models.pagination.SortOrder;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
-import org.springframework.data.elasticsearch.core.query.Query;
+import org.springframework.data.elasticsearch.core.query.Criteria;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -13,54 +11,42 @@ public interface ElasticSearchableRepository<T, ID> {
 
     List<T> search(SearchQuery query, Class<T> type);
 
-    default Query convertIntoSearchQuery(SearchQuery query, String...skipKeys) {
-        Query mQuery = new NativeSearchQueryBuilder().build();
+    default List<Criteria> getCriteriaList(SearchQuery query, String...skipKeys) {
+        List<Criteria> criteriaList = new ArrayList<>();
         List<String> skipList = Arrays.asList(skipKeys);
-        /*query.getProperties().stream()
+        query.getProperties().stream()
                 .filter(prop -> !skipList.contains(prop.getKey()))
                 .filter(prop -> prop.getKey() != null)
                 .forEach(prop -> {
                     switch (prop.getOperator()){
                         case GREATER_THAN:
-                            mQuery.addCriteria(Criteria.where(prop.getKey()).gt(prop.getValue()));
+                            criteriaList.add(Criteria.where(prop.getKey()).greaterThan(prop.getValue()));
                             break;
                         case GREATER_THAN_OR_EQUAL:
-                            mQuery.addCriteria(Criteria.where(prop.getKey()).gte(prop.getValue()));
+                            criteriaList.add(Criteria.where(prop.getKey()).greaterThanEqual(prop.getValue()));
                             break;
                         case LESS_THAN:
-                            mQuery.addCriteria(Criteria.where(prop.getKey()).lt(prop.getValue()));
+                            criteriaList.add(Criteria.where(prop.getKey()).lessThan(prop.getValue()));
                             break;
                         case LESS_THAN_OR_EQUAL:
-                            mQuery.addCriteria(Criteria.where(prop.getKey()).lte(prop.getValue()));
+                            criteriaList.add(Criteria.where(prop.getKey()).lessThanEqual(prop.getValue()));
                             break;
                         case IN:
                             Object[] inValues = prop.getValue()
                                     .replace("'", "")
                                     .split(",");
-                            mQuery.addCriteria(Criteria.where(prop.getKey()).in(inValues));
+                            criteriaList.add(Criteria.where(prop.getKey()).in(inValues));
                             break;
                         case LIKE:
-                            mQuery.addCriteria(Criteria.where(prop.getKey()).regex(prop.getValue()));
+                            criteriaList.add(Criteria.where(prop.getKey()).contains(prop.getValue()));
                             break;
                         case IS_NULL:
-                            mQuery.addCriteria(Criteria.where(prop.getKey()).is(null));
+                            criteriaList.add(Criteria.where(prop.getKey()).is(null));
                             break;
                         default:
-                            mQuery.addCriteria(Criteria.where(prop.getKey()).is(prop.getValue()));
+                            criteriaList.add(Criteria.where(prop.getKey()).is(prop.getValue()));
                     }
                 });
-        if (!query.getDescriptors().isEmpty()){
-            query.getDescriptors().stream()
-                    .filter(descriptor -> descriptor.getKeys().size() > 0)
-                    .forEach(descriptor -> {
-                Sort.Direction direction = (descriptor.getOrder() == SortOrder.ASC)
-                        ? Sort.Direction.ASC
-                        : Sort.Direction.DESC;
-                mQuery.with(Sort.by(direction
-                        , descriptor.getKeys().toArray(new String[0]))
-                );
-            });
-        }*/
-        return mQuery;
+        return criteriaList;
     }
 }
--- src/main/java/com/infoworks/lab/domain/repositories/impl/ElasticSearchableRepositoryImpl.java
+++ src/main/java/com/infoworks/lab/domain/repositories/impl/ElasticSearchableRepositoryImpl.java
@@ -2,7 +2,15 @@ package com.infoworks.lab.domain.repositories.impl;
 
 import com.infoworks.lab.domain.repositories.ElasticSearchableRepository;
 import com.infoworks.lab.rest.models.SearchQuery;
+import com.infoworks.lab.rest.models.pagination.SortOrder;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.SearchHitSupport;
+import org.springframework.data.elasticsearch.core.SearchHits;
+import org.springframework.data.elasticsearch.core.query.Criteria;
+import org.springframework.data.elasticsearch.core.query.CriteriaQuery;
+import org.springframework.data.elasticsearch.core.query.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -19,17 +27,37 @@ public class ElasticSearchableRepositoryImpl<T, ID> implements ElasticSearchable
 
     @Override
     public List<T> search(SearchQuery query, Class<T> type) {
-        /*Query mQuery = convertIntoSearchQuery(query);
+        List<Criteria> criteriaList = getCriteriaList(query);
         //If-Query-Is-Empty: return empty list;
-        if (mQuery.getQueryObject().isEmpty()) return new ArrayList<>();
+        if (criteriaList.isEmpty()) return new ArrayList<>();
+        //
+        Criteria searchCriteria = new Criteria();
+        //FIXME: not working
+        criteriaList.forEach(criteria -> searchCriteria.or(criteria));
+        Query mQuery = new CriteriaQuery(searchCriteria);
+        if (!query.getDescriptors().isEmpty()){
+            query.getDescriptors().stream()
+                    .filter(descriptor -> descriptor.getKeys().size() > 0)
+                    .forEach(descriptor -> {
+                        Sort.Direction direction = (descriptor.getOrder() == SortOrder.ASC)
+                                ? Sort.Direction.ASC
+                                : Sort.Direction.DESC;
+                        mQuery.addSort(Sort.by(direction, descriptor.getKeys().toArray(new String[0]))
+                        );
+                    });
+        }
+        //If-Query-Is-Empty: return empty list;
+        if (mQuery.getFields().isEmpty()) return new ArrayList<>();
         int page = query.getPage();
         int size = query.getSize();
         if (page < 0) page = 0;
         if (size <= 0) size = 10;
-        mQuery.with(PageRequest.of(page, size));
-        List<T> iterable = template.find(mQuery, type);
-        return iterable;*/
-        return new ArrayList<>();
+        mQuery.setPageable(PageRequest.of(page, size));
+        SearchHits<T> iterable = template.search(mQuery, type);
+        //SearchPage<T> pages = SearchHitSupport.searchPageFor(iterable, mQuery.getPageable());
+        @SuppressWarnings("unchecked")
+        List<T> items = (List<T>) SearchHitSupport.unwrapSearchHits(iterable);
+        return items;
     }
 
 }
